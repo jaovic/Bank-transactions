7a037d21c583ceb75f88b8e6149eb8263468d2e6
João Victor Martins <j_victorfernandes@hotmail.com>
2022-08-23 13:19:40 -0300

    teste

--- user/src/repository/repository.module.ts
+++ user/src/repository/repository.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { RepositoryService } from './repository.service';
 
 @Module({
-  providers: [RepositoryService]
+  providers: [RepositoryService],
+  exports: [RepositoryService],
 })
 export class RepositoryModule {}
