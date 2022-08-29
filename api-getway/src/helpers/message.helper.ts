f849c491540f0cc2cdb4298c2354014462198572
João Victor Martins <j_victorfernandes@hotmail.com>
2022-08-29 13:16:40 -0300

    ✨ front validation to class-validator

---
+++ api-getway/src/helpers/message.helper.ts
@@ -0,0 +1,22 @@
+export const MessagesHelper = {
+  PAYERACCOUNTID_REQUIRED: 'payerAccountId is required',
+  PAYERACCOUNTID_INVALID: 'payerAccountId is invalid',
+
+  RECEIVINGACCOUNTID_REQUIRED: 'receivingAccountId is required',
+  RECEIVINGACCOUNTID_INVALID: 'receivingAccountId is invalid',
+
+  VALUE_REQUIRED: 'Value is required',
+  VALUE_INVALID: 'Value is invalid',
+
+  PAYERAGENCY_REQUIRED: 'payerAgency is required',
+  PAYERAGENCY_INVALID: 'payerAgency is invalid',
+
+  PAYERBANK_REQUIRED: 'payerBank is required',
+  PAYERBANK_INVALID: 'payerBank is invalid',
+
+  RECEIVINGAGENCY_REQUIRED: 'receivingAgency is required',
+  RECEIVINGAGENCY_INVALID: 'receivingAgency is invalid',
+
+  ECEIVINGBANK_REQUIRED: 'receivingBank is required',
+  ECEIVINGBANK_INVALID: 'receivingBank is invalid',
+};
