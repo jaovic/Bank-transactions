1167afc034c5228225aaf3047e2b84717174ff7d
João Victor Martins <j_victorfernandes@hotmail.com>
2022-08-31 17:04:09 -0300

    ✨ add save client on db to consume kafka

--- bank/src/repository/repository.service.ts
+++ bank/src/repository/repository.service.ts
@@ -1,11 +1,28 @@
 import { Injectable } from '@nestjs/common';
 
 import { PrismaClient } from '@prisma/client';
+import { newClientDto } from 'src/user/dto/new.client.dto';
 
 const prisma = new PrismaClient();
 
 @Injectable()
 export class RepositoryService {
+  async saveClient(body: newClientDto) {
+    try {
+      return await prisma.user.create({
+        data: {
+          email: body.email,
+          name: body.name,
+          phone: body.phone,
+          cpf: body.cpf,
+          accountId: body.id,
+        },
+      });
+    } catch (error: any) {
+      throw new Error(`Prisma Error: ${error}`);
+    }
+  }
+
   async findByEmail(email: string) {
     try {
       return await prisma.user.findUnique({
@@ -20,6 +37,16 @@ export class RepositoryService {
     try {
       return await prisma.user.findUnique({
         where: { id },
+      });
+    } catch (error) {
+      throw new Error(`Prisma Error: ${error}`);
+    }
+  }
+
+  async findByAccountId(id: string) {
+    try {
+      return await prisma.user.findUnique({
+        where: { accountId: id },
       });
     } catch (error) {
       throw new Error(`Prisma Error: ${error}`);
