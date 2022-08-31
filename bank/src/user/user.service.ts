f55c92288ed440356bff0eb164baac380fd93f6e
João Victor Martins <j_victorfernandes@hotmail.com>
2022-08-31 17:04:48 -0300

    ✨ add service logic bank

--- bank/src/user/user.service.ts
+++ bank/src/user/user.service.ts
@@ -1,4 +1,26 @@
+import { depositDto } from './dto/deposit.dto';
+import { RepositoryService } from './../repository/repository.service';
 import { Injectable } from '@nestjs/common';
+import { newClientDto } from './dto/new.client.dto';
+import { tedPaymentoDto } from './dto/ted.payment.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly repository: RepositoryService) {}
+
+  async getClient(body: newClientDto) {
+    await this.repository.saveClient(body);
+  }
+
+  async getPayments(body: tedPaymentoDto) {
+    console.log(body.body.payerAccountId);
+    const data = await this.repository.findByAccountId(
+      body.body.payerAccountId,
+    );
+    console.log(data);
+  }
+
+  async deposit(body: depositDto) {
+    console.log(body.body);
+  }
+}
